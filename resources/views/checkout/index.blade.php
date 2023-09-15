9d7bb2e63ecf5b48694ef2789a3c5680b01666f7
Luan Fernandes <luanfern2004@gmail.com>
2023-09-14 22:38:00 -0300

    input and label custom components and form

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -26,6 +26,48 @@
                     </dl>
                 </div>
             </section>
+
+            <section aria-labelledby="payment-and-shipping-heading"
+                class="py-16 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:w-full lg:max-w-lg lg:pb-24 lg:pt-0">
+                <form class="mx-auto max-w-2xl px-4 lg:max-w-none lg:px-0">
+                    <div>
+                        <x-section-title title="Informações de contato" />
+                        <div class="mt-6">
+                            <x-input-label for="email-address" value="Email" />
+                            <div class="mt-1">
+                                <x-text-input type="email" name="email" id="email-address" autocomplete="email"
+                                    placeholder="Digite seu E-mail" />
+                            </div>
+                        </div>
+                        <div class="mt-10">
+                            <x-section-title title="Detalhes do Pagamento" />
+                            <div class="mt-6 grid grid-cols-3 gap-x-4 gap-y-6 sm:grid-cols-4">
+                                <div class="col-span-3 sm:col-span-4">
+                                    <x-input-label for="card-number" value="Numero do Cartão" />
+                                    <div class="mt-1">
+                                        <x-text-input type="text" name="card-number" id="card-number"
+                                            placeholder="Numero do cartao" />
+                                    </div>
+                                </div>
+                                <div class="cols-span-2 sm:col-span-3">
+                                    <x-input-label for="expiration-date" value="Data de Expiração" />
+                                    <div class="mt-1">
+                                        <x-text-input type="text" name="expiration-date" id="expiration-date"
+                                            placeholder="MM/AA" />
+                                    </div>
+                                </div>
+                                <div>
+                                    <x-input-label for="cvc" value="CVC" />
+                                    <div class="mt-1">
+                                        <x-text-input type="text" name="cvc" id="cvc"
+                                            placeholder="CVC" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </section>
         </div>
     </div>
 </x-app-layout>
